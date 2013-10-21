ba50d1cea6dbce7cba17fa32a5c4fa2d5595ca78
sandinurahmat777 <sandinurahmat@gmail.com>
2013-10-21 19:36:23 +0700

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,15 +8,31 @@ describe "Static pages" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-    #it { should have_title(full_title('')) }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
   end
 
   describe "Help page" do
     before { visit help_path }
 
     it { should have_content('Help') }
-    #it { should have_title(full_title('Help')) }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
@@ -30,6 +46,6 @@ describe "Static pages" do
     before { visit contact_path }
 
     it { should have_content('Contact') }
-    #it { should have_title(full_title('Contact')) }
+    it { should have_title(full_title('Contact')) }
   end
 end
